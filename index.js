7ef4d1d74ff447a3109d527b0809d11532e0fafe
Quirin Brauer <quirin.brauer@web.de>
2022-05-02 18:33:00 +0200

    addedvisulaconsole

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 scanButton= document.getElementById("scanbutton");
+
+window.onload=init;
+function init()
+{
+    document.getElementById("console").innerHTML+="init";
+}
+
 function nfcAction()
 {
 
@@ -7,12 +14,15 @@ function nfcAction()
       console.log("Scan started successfully.");
       ndef.onreadingerror = () => {
         console.log("Cannot read data from the NFC tag. Try another one?");
+        document.getElementById("console").innerHTML+="Cannot read data from the NFC tag. Try another one?";
       };
       ndef.onreading = event => {
         console.log("NDEF message read.");
+        document.getElementById("console").innerHTML+="NDEF message read.";
       };
     }).catch(error => {
       console.log(`Error! Scan failed to start: ${error}.`);
+      document.getElementById("console").innerHTML+="Error! Scan failed to start: ${error}.";
     });
     
  
